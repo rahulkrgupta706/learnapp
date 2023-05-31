46c7b3578dcf6ab3bd7a9ac85135283a85d66773
rahulkrgupta706 <rahulkrgupta706@gmail.com>
2023-05-31 10:38:02 +0530

    First Commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+import 
+
+function App() {
+  return (
+    //<UseState/>
+    
+    <PackingList/>
+}
+
+export default App;
---
+++ src/components/eventHandler.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+function EventHandler() {
+    
+    // const func =() => {
+    //     console.log('button')
+    // }
+    return (
+        <>
+        {/*<button>onClick={()=> {console.log("button click")}click me}</button>*/}
+        </>
+    )
+}
+
+export default EventHandler
